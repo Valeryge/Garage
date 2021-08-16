7fecf8c4869310208e96d38cbf6ff0ca4dab6e09
Valery Geber <valeryge@mta.ac.il>
2021-08-16 22:46:01 +0300

    engines without inheritence

--- Ex03.GarageLogic/ElectricEngine.cs
+++ Ex03.GarageLogic/ElectricEngine.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace Ex03.GarageLogic
 {
-    public class ElectricEngine : Engine
+    public class ElectricEngine
     {
-        public ElectricEngine(float i_TankSize, float i_CurrentCapacity): base(i_TankSize, i_CurrentCapacity)
+        private float m_WorkingHoursLeft;
+        private float m_MaxBatteryLife;
+
+        public ElectricEngine(float i_TankSize, float i_CurrentCapacity)
         {
-            
+            m_MaxBatteryLife = i_TankSize;
+            m_WorkingHoursLeft = i_CurrentCapacity;
+        }
+
+        public float GetEnergyPercent()
+        {
+            return m_WorkingHoursLeft / m_MaxBatteryLife * 100;
+        }
+
+        public void Charge(float i_HoursToCharge)
+        {
+            if (i_HoursToCharge + m_WorkingHoursLeft <= m_MaxBatteryLife)
+            {
+                m_WorkingHoursLeft += i_HoursToCharge;
+            } else
+            {
+                //TODO: Expetion
+            }
         }
     }
 }
--- Ex03.GarageLogic/GasEngine.cs
+++ Ex03.GarageLogic/GasEngine.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace Ex03.GarageLogic
 {
-    public class GasEngine : Engine
+    public class GasEngine
     {
-        public GasEngine(float i_TankSize, float i_CurrentCapacity) : base(i_TankSize, i_CurrentCapacity)
-        {
+        private VehicleFactory.FuelType m_FuelType;
+        private float m_CurrentCapacity;
+        private float m_TankSize;
 
+        public GasEngine(float i_TankSize, float i_CurrentCapacity, VehicleFactory.FuelType i_FuelType)
+        {
+            m_CurrentCapacity = i_CurrentCapacity;
+            m_TankSize = i_TankSize;
+            m_FuelType = i_FuelType;
+        }
+
+        public float GetEnergyPercent()
+        {
+            return m_CurrentCapacity / m_TankSize * 100;
+        }
+
+        public void AddFuel(float i_FuelAmount, VehicleFactory.FuelType i_Type)
+        {
+            if (i_FuelAmount + m_CurrentCapacity <= m_TankSize && i_Type == m_FuelType)
+            {
+                m_CurrentCapacity += i_FuelAmount;
+            } else
+            {
+                // TODO: Expetion
+            }
         }
     }
 }
